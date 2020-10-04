698037ec199e56a63737a439ccc0797649660276
biggestx <sojilo2556@gmail.com>
2020-10-04 21:24:07 +0900

    compile -> json -> zeroformatter 흐름 구성

--- ExcelExporter/ExcelExporter.cs
+++ ExcelExporter/ExcelExporter.cs
@@ -32,29 +32,50 @@ namespace ExcelExporter
         namespace Table
         {{
             // class
-            [ZeroFormattable]
-            [Serializable]
-            class {0} 
+            
+            public class {0} 
             {{
                 // data
-                class {1}
+                [ZeroFormattable]
+                public class {1}
                 {{
                     {2}
                 }}
 
                 // Container
-                {3} 
+                {3} Container = new {3}();
 
                 private void Deserialize()
                 {{
-                    //string path = {4};
-                    //var load = File.ReadAllText(path);
-                    //Container = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<int,{1}>>(load);
-                    //Console.WriteLine(data);
-                }}  
-                private void Print()
+                    string path = {4};
+                    var load = System.IO.File.ReadAllBytes(path);
+                    var test = ZeroFormatterSerializer.Deserialize<{1}>(load);
+                    Console.WriteLine(test.ID);
+
+                }}
+
+                public void MakeSerializedFile(string txt)
                 {{
-                    Console.WriteLine({4});
+                    Console.WriteLine(txt);
+                    var test = new {1}();
+                    try
+                    {{
+                        var container = Newtonsoft.Json.JsonConvert.DeserializeObject<{3}>(txt);
+                        foreach(var data in container)
+                        {{
+                            Console.WriteLine(data.Value.ID);
+                        }}
+                        
+                        var bytes = ZeroFormatterSerializer.Serialize(test);
+                        System.IO.File.WriteAllBytes({4}, bytes);
+                        
+                    }}
+                    catch(Exception e)
+                    {{
+                        Console.WriteLine(e.Message);
+                    }}
+
+                    Deserialize();
                 }}
             }}
         
@@ -64,33 +85,9 @@ namespace ExcelExporter
 
         ";
 
-        [ZeroFormatter.ZeroFormattable]
-        public class TEMP
-        {
-            [ZeroFormatter.Index(0)]
-            public int ID;
-
-            [ZeroFormatter.Index(1)]
-            public int Power;
-
-            [ZeroFormatter.Index(2)]
-            public string Desc;
-        }
-
-
-        public void Test()  
-        {
-            var load = File.ReadAllText(@"D:\git repository\ExcelExporter\ExcelExporter\Test.json");
-            var data = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<int,TEMP>>(load);
-            Console.WriteLine(data);
-
-        }
-
 
         public void Export()
         {
-            //Test();
-            
             string path = @"D:\git repository\ExcelExporter\ExcelExporter\";
 
             string excelPath = path + "Test.xlsx";
@@ -130,7 +127,10 @@ namespace ExcelExporter
                     var fieldName = worksheet.Cells[1, i].Value;
                     var @type = worksheet.Cells[2, i].Value;
 
-                    fields += "public " + @type + " " + fieldName + ";\n";
+                    fields += 
+                        $"[Index({i-1})]" +
+                        "public virtual " + @type + " " + fieldName +
+                        "{get;set;}" + "\n";
                     fieldNames.Add(fieldName);
                     switch (type)
                     {
@@ -149,7 +149,6 @@ namespace ExcelExporter
                 for (int i = 3; i <= rowCount; ++i)
                 {
                     dynamic b = new ExpandoObject();
-                    System.ComponentModel.TypeDescriptor.AddAttributes(b, new ZeroFormatter.ZeroFormattableAttribute());
                     for (int j = 1; j <= colCount; ++j)
                     {
                         Excel.Range cell = worksheet.Cells[i, j];
@@ -174,15 +173,15 @@ namespace ExcelExporter
 
                 }
 
-                //var jsonFile = ZeroFormatter.ZeroFormatterSerializer.Serialize(values);
-                //System.IO.File.WriteAllBytes(jsonPath, jsonFile);
+                var jsonFile = Newtonsoft.Json.JsonConvert.SerializeObject(values);
+                System.IO.File.WriteAllText(jsonPath, jsonFile);
 
 
                 var containerTypeCell = worksheet.Cells[2, 1].Value as string;
 
                 var container = 
                     string.Format(
-                        "Dictionary<{0},{1}> Container = new Dictionary<{0},{1}>();\n",
+                        "Dictionary<{0},{1}>\n",
                         containerTypeCell,
                         fileName+"Data");
 
@@ -193,7 +192,7 @@ namespace ExcelExporter
                     dataName,
                     fields,
                     container,
-                    "@" + QUOTE + path + QUOTE
+                    "@" + QUOTE + path +"Test.byte" + QUOTE
                     );
 
                 file = Microsoft.CodeAnalysis.CSharp.CSharpSyntaxTree.ParseText(file).GetRoot().NormalizeWhitespace().ToFullString();
@@ -201,13 +200,14 @@ namespace ExcelExporter
 
                 System.IO.File.WriteAllText(csPath, file);
 
-
                 // compile to export json 
                 System.CodeDom.Compiler.CodeDomProvider codeDom = System.CodeDom.Compiler.CodeDomProvider.CreateProvider("CSharp");
                 System.CodeDom.Compiler.CompilerParameters cparams = new System.CodeDom.Compiler.CompilerParameters();
                 cparams.GenerateInMemory = true;
                 cparams.ReferencedAssemblies.Add(@"D:\git repository\ExcelExporter\packages\ZeroFormatter.1.6.4\lib\net45\ZeroFormatter.dll");
                 cparams.ReferencedAssemblies.Add(@"D:\git repository\ExcelExporter\packages\ZeroFormatter.Interfaces.1.6.4\lib\net45\ZeroFormatter.Interfaces.dll");
+                cparams.ReferencedAssemblies.Add(@"D:\git repository\ExcelExporter\packages\Newtonsoft.Json.12.0.3\lib\net45\Newtonsoft.Json.dll");
+
                 System.CodeDom.Compiler.CompilerResults results = codeDom.CompileAssemblyFromSource(cparams, file);
                 if (results.Errors.Count > 0)
                 {
@@ -217,10 +217,16 @@ namespace ExcelExporter
                     }
                     return;
                 }
-                Type myType = results.CompiledAssembly.GetType("Test.MyClass");
+                Type myType = results.CompiledAssembly.GetType("Table.TestTable");
                 object myObject = Activator.CreateInstance(myType);
-                MethodInfo mi = myObject.GetType().GetMethod("PrintName");
-                mi.Invoke(myObject, null);
+                MethodInfo mi = myObject.GetType().GetMethod("MakeSerializedFile");
+                
+                // TODO 
+                // 1. json 보내기
+                // 2. 받은 json으로 Deserialization
+                // 3. ZeroFormatter로 Serialization
+                // 4. Unity 에서 사용할 수 있게 using newtonsoft, deserialization 메서드 제거
+                mi.Invoke(myObject,new object[] { jsonFile, });
             }
             catch (Exception ex)
             {
