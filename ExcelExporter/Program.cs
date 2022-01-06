d61a12e7c29c5e45faa626b7357c2facaa6d0b7d
biggestx <sojilo2556@gmail.com>
2022-01-06 14:53:16 +0900

    apply export all at main

--- ExcelExporter/Program.cs
+++ ExcelExporter/Program.cs
@@ -12,11 +12,37 @@ namespace ExcelExporter
         {
             ExcelExporter e = new ExcelExporter();
 
-            //string path = @"D:\git repository\ExcelExporter\ExcelExporter\Test.xlsx";
-            //e.Export(path);
+            string defaultInputDirectory = System.IO.Directory.GetCurrentDirectory() + "\\Files_new";
+            string defaultOutputDirectory = System.IO.Directory.GetCurrentDirectory() + "\\Files_new\\Exported";
 
-            string path = "Files";
-            e.ExportAll(path);
+            string inputDirectory = defaultInputDirectory;
+            string outputDirectory = defaultOutputDirectory;
+
+            if (args.Length >= 2)
+            {
+                inputDirectory = args[0];
+                outputDirectory = args[1];
+            }
+            else
+            {
+                inputDirectory = defaultInputDirectory;
+                outputDirectory = defaultOutputDirectory;
+            }
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("[ExcelExporter] \n");
+            Console.WriteLine($"- Input Directory  : {inputDirectory}");
+            Console.WriteLine($"- Output Directory : {outputDirectory}");
+            Console.WriteLine("-----------------------");
+            if (System.IO.Directory.Exists(inputDirectory) == false)
+                System.IO.Directory.CreateDirectory(inputDirectory);
+
+            if (System.IO.Directory.Exists(outputDirectory) == false)
+                System.IO.Directory.CreateDirectory(outputDirectory);
+
+            e.ExportAll(inputDirectory,outputDirectory);
+
+            System.Threading.Thread.Sleep(2000);
         }
     }
 }
